3b0a14b55abe1c00dbcebef1f7fc8594339a4bb6
runoneall <runoobsteve@hotmail.com>
2026-01-09 08:24:45 +0800

    better cmd arg

--- cmd/initCmdArg.go
+++ cmd/initCmdArg.go
@@ -11,11 +11,11 @@ var (
 )
 
 func initCmdArg() {
-	IsStartServer = Cli.Flags().Bool("s", false, "启动后端服务")
+	IsStartServer = Cli.Flags().BoolP("server", "s", false, "启动后端服务")
 	RemoteHost = Cli.Flags().String("rh", "0.0.0.0", "远程主机")
 	RemotePort = Cli.Flags().Int("rp", 0, "远程端口")
 	LocalHost = Cli.Flags().String("lh", "0.0.0.0", "本地主机")
 	LocalPort = Cli.Flags().Int("lp", 0, "本地端口")
-	DoStop = Cli.Flags().String("stop", "", "停止转发")
-	IsList = Cli.Flags().Bool("l", false, "列出转发的端口")
+	DoStop = Cli.Flags().StringP("stop", "S", "", "停止转发")
+	IsList = Cli.Flags().BoolP("list", "l", false, "列出转发的端口")
 }
